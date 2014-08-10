09d3a1f1d8e12ae8b4d64511c129da5721d0b477
gusakovK94 <gusakovK94@gmail.com>
2014-08-10 20:04:59 +0400

    Added some new models

---
+++ src/main/java/com/aboutme/blog/json/Auth.java
@@ -0,0 +1,9 @@
+package com.aboutme.blog.json;
+
+public class Auth {
+
+	public String errorName;
+	public String errorMessage;
+	public String redirectUrl;
+
+}
---
+++ src/main/java/com/aboutme/blog/models/Admin.java
@@ -0,0 +1,68 @@
+package com.aboutme.blog.models;
+
+public class Admin {
+
+	private int id;
+	private String username;
+	private String surname;
+	private String login;
+	private String password;
+	private int age;
+	private String profileImageUrl;
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void setProfileImageUrl(String profileImageUrl) {
+		this.profileImageUrl = profileImageUrl;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getProfileImageUrl() {
+		return profileImageUrl;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+}
