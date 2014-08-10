4aab3e214c0aa0aecc1eb144b756cbca8ea8c196
gusakovK94 <gusakovK94@gmail.com>
2014-08-10 20:05:33 +0400

    Added admin service

---
+++ src/main/java/com/aboutme/blog/service/AdminService.java
@@ -0,0 +1,26 @@
+package com.aboutme.blog.service;
+
+import com.aboutme.blog.dao.interfaces.AdminUserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AdminService {
+
+	private AdminUserDAO adminDAO;
+
+	public AdminService() {
+
+	}
+
+	@Autowired
+	public void setAdminDAO(AdminUserDAO adminDAO) {
+		this.adminDAO = adminDAO;
+	}
+
+	public boolean authenticate(String login, String password) {
+		if (adminDAO.find(login, password).isEmpty()) {
+			return false;
+		}
+		return true;
+	}
+
+}
