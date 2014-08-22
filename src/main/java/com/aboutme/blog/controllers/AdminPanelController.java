355e6e8bbdcfa3f0ffc26758f3d43a162b19e88a
gusakovK94 <gusakovK94@gmail.com>
2014-08-23 00:40:24 +0400

    Some changes

--- src/main/java/com/aboutme/blog/controllers/AdminPanelController.java
+++ src/main/java/com/aboutme/blog/controllers/AdminPanelController.java
@@ -3,10 +3,10 @@ package com.aboutme.blog.controllers;
 import com.aboutme.blog.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping(value = "/admin")
@@ -27,9 +27,21 @@ public class AdminPanelController {
 	@RequestMapping(method = RequestMethod.POST)
 	@ResponseBody
 	public boolean authRequest(@RequestParam("login") String login,
-								   	@RequestParam("password") String password) {
+								   	@RequestParam("password") String password, HttpSession httpSession) {
 
-		return adminService.authenticate(login, password);
+		if (adminService.authenticate(login, password)) {
+			httpSession.setAttribute("admin", login);
+			return true;
+		}
+		return false;
+	}
+
+	@RequestMapping(value = "/main", method = RequestMethod.GET)
+	public String authSuccess(HttpServletRequest request) {
+		if (request.getSession(true).getAttribute("admin") == null) {
+			return "redirect:/admin";
+		}
+		return "admin/index";
 	}
 
 }
