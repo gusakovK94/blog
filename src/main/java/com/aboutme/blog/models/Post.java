f902c99bffae02d5447b7ee7e58ad4c9342ba437
gusakovK94 <gusakovK94@gmail.com>
2014-09-03 18:30:38 +0400

    refactored post model

--- src/main/java/com/aboutme/blog/models/Post.java
+++ src/main/java/com/aboutme/blog/models/Post.java
@@ -4,12 +4,22 @@ import java.util.Date;
 
 public class Post {
 
+	private int id;
 	private String topic;
 	private String author;
 	/**/private String tag;
 	private String image;
 	private String content;
 	private Date date;
+	private String description;
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
 	public void setTopic(String topic) {
 		this.topic = topic;
@@ -33,6 +43,14 @@ public class Post {
 
 	public void setDate(Date date) {
 		this.date = date;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getDescription() {
+		return description;
 	}
 
 	public String getTopic() {
