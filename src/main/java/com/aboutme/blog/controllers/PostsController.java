7d652326aca41d70b1a397581e2b3822c7c7e94a
gusakovK94 <gusakovK94@gmail.com>
2014-09-03 18:36:43 +0400

    refactored post settings

--- src/main/java/com/aboutme/blog/controllers/PostsController.java
+++ src/main/java/com/aboutme/blog/controllers/PostsController.java
@@ -5,40 +5,79 @@ import com.aboutme.blog.service.PostsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
 
 @Controller
-@RequestMapping(value = "/admin/posts")
 public class PostsController {
 
 	private PostsService postsService;
+	private static final double POSTS_ON_PAGE = 7;
+	private double postsCount;
 
 	@Autowired
 	public void setPostsService(PostsService postsService) {
 		this.postsService = postsService;
+		postsCount = postsService.getAllPosts().size();
 	}
 
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value = "admin/posts", method = RequestMethod.GET)
 	public String showMainPostPage(Model model, HttpSession httpSession) {
 		if (httpSession.getAttribute("admin") == null) {
 			return "admin/auth";
 		} else {
 			model.addAttribute("post", new Post());
+			model.addAttribute("posts", postsService.getPosts(0));
+			model.addAttribute("pagsize", Math.ceil(postsCount/POSTS_ON_PAGE));
 			return "admin/posts";
 		}
 	}
 
-	@RequestMapping(method = RequestMethod.POST)
+	@RequestMapping(value = "/admin/posts/pages/{page}", method = RequestMethod.GET)
+	public String showCurrentPostPage(Model model, HttpSession httpSession, @PathVariable int page) {
+		if (httpSession.getAttribute("admin") == null) {
+			return "admin/auth";
+		} else {
+			model.addAttribute("post", new Post());
+			model.addAttribute("posts", postsService.getPosts((page - 1)*7));
+			model.addAttribute("pagsize", Math.ceil(postsCount/POSTS_ON_PAGE));
+			return "admin/posts";
+		}
+	}
+
+	@RequestMapping(value = "/admin/posts", method = RequestMethod.POST)
 	public String savePost(@ModelAttribute Post post, @RequestParam(value = "tag", required = false) String tags,
 						   @RequestParam(value = "photo",required = false) MultipartFile photo) {
 
 		postsService.savePost(post, tags, photo);
+		postsCount++;
+		return "redirect:/admin/posts";
+	}
+
+	@RequestMapping(value = "/admin/post/edit/{post_id}", method = RequestMethod.POST)
+	public String editPost(@PathVariable int post_id, @RequestParam("topic") String topic,
+						   	@RequestParam("author") String author, @RequestParam("description") String description,
+							@RequestParam("content") String content, @RequestParam("imageurl") String imageurl,
+							@RequestParam("photo") MultipartFile image) {
+
+		Post post = new Post();
+		post.setId(post_id);
+		post.setTopic(topic);
+		post.setAuthor(author);
+		post.setDescription(description);
+		post.setContent(content);
+		post.setImage(imageurl);
+		String tags = null;
+		postsService.updatePost(post, tags, image);
+		return "redirect:/admin/posts";
+	}
+
+	@RequestMapping(value = "/admin/post/delete/{post_id}", method = RequestMethod.POST)
+	public String deletePost(@PathVariable int post_id) {
+		postsService.deletePost(post_id);
+		postsCount--;
 		return "redirect:/admin/posts";
 	}
 
--- src/main/java/com/aboutme/blog/dao/PostsDAOImpl.java
+++ src/main/java/com/aboutme/blog/dao/PostsDAOImpl.java
@@ -2,15 +2,22 @@ package com.aboutme.blog.dao;
 
 import com.aboutme.blog.dao.interfaces.PostsDao;
 import com.aboutme.blog.models.Post;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class PostsDAOImpl extends JdbcDaoSupport
 							implements PostsDao {
 
-	private static final String SQL_INSERT_POST = "INSERT INTO posts (topic, image, author, content, date) " +
-			"VALUES (?, ?, ?, ?, ?)";
+	private static final String SQL_INSERT_POST = "INSERT INTO posts (topic, image, author, content, date, description) " +
+			"VALUES (?, ?, ?, ?, ?, ?)";
+	private static final String SQL_UPDATE_POST = "UPDATE posts SET topic = ?, image = ?, author = ?, content = ?, date = ?, description = ?" +
+			"WHERE id = ?";
+	private static final String SQL_DELETE_POST = "DELETE FROM posts WHERE id = ?";
+
 	@Override
 	public void add(Post post) {
 		getJdbcTemplate().update(SQL_INSERT_POST,
@@ -18,7 +25,8 @@ public class PostsDAOImpl extends JdbcDaoSupport
 								post.getImage(),
 								post.getAuthor(),
 								post.getContent(),
-								post.getDate());
+								post.getDate(),
+								post.getDescription());
 	}
 
 	@Override
@@ -28,16 +36,60 @@ public class PostsDAOImpl extends JdbcDaoSupport
 
 	@Override
 	public void update(Post post) {
-
+		getJdbcTemplate().update(SQL_UPDATE_POST,
+								post.getTopic(),
+								post.getImage(),
+								post.getAuthor(),
+								post.getContent(),
+								post.getDate(),
+								post.getDescription(),
+								post.getId());
 	}
 
 	@Override
-	public void delete(Post post) {
-
+	public void delete(int post_id) {
+		getJdbcTemplate().update(SQL_DELETE_POST,
+								post_id);
 	}
 
 	@Override
-	public List<Post> getPosts() {
-		return null;
+	public List<Post> getPosts(int offset) {
+		List<Post> posts = getJdbcTemplate().query(
+				"SELECT * FROM posts ORDER BY id DESC LIMIT 7 OFFSET " + offset ,
+				new RowMapper<Post>() {
+					public Post mapRow(ResultSet rs, int rowNum) throws SQLException {
+						Post post = new Post();
+						post.setId(rs.getInt("id"));
+						post.setTopic(rs.getString("topic"));
+						post.setAuthor(rs.getString("author"));
+						post.setImage(rs.getString("image"));
+						post.setContent(rs.getString("content"));
+						post.setDate(rs.getDate("date"));
+						post.setDescription(rs.getString("description"));
+						return post;
+					}
+				});
+
+		return posts;
+	}
+
+	public List<Post> getAllPosts() {
+		List<Post> posts = getJdbcTemplate().query(
+				"SELECT * FROM posts",
+				new RowMapper<Post>() {
+					public Post mapRow(ResultSet rs, int rowNum) throws SQLException {
+						Post post = new Post();
+						post.setId(rs.getInt("id"));
+						post.setTopic(rs.getString("topic"));
+						post.setAuthor(rs.getString("author"));
+						post.setImage(rs.getString("image"));
+						post.setContent(rs.getString("content"));
+						post.setDate(rs.getDate("date"));
+						post.setDescription(rs.getString("description"));
+						return post;
+					}
+				});
+
+		return posts;
 	}
 }
